7e0432dee6c898fff4de06cd10a8645f1cb20225
Shurakip <and.u8230@gmail.com>
2023-04-18 11:14:39 +0300

    Сохранение ради сохранения

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,51 +1,51 @@
-﻿//                                          Задача 1
-Console.Write("Введите первое число: ");
-int num1 = int.Parse(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int num2 = int.Parse(Console.ReadLine());
+﻿// //                                          Задача 1
+// Console.Write("Введите первое число: ");
+// int num1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = int.Parse(Console.ReadLine());
 
-if (num1 > num2)
-   Console.WriteLine($"{num1} больше {num2}");
-else
-    Console.WriteLine($"{num2} больше {num1}");
+// if (num1 > num2)
+//    Console.WriteLine($"{num1} больше {num2}");
+// else
+//     Console.WriteLine($"{num2} больше {num1}");
 
-//                                          Задача 2
-Console.Write("Введите первое число: ");
-int num1 = int.Parse(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int num2 = int.Parse(Console.ReadLine());
-Console.Write("Введите третье число: ");
-int num3 = int.Parse(Console.ReadLine());
+// //                                          Задача 2
+// Console.Write("Введите первое число: ");
+// int num1 = int.Parse(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int num2 = int.Parse(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int num3 = int.Parse(Console.ReadLine());
 
-int max = num1;
+// int max = num1;
 
-if (num2 > max)
-{
-    max = num2;
-}
-if (num3 > max)
-{
-    max = num3;
-}
-Console.WriteLine("Максимальное число: " +max);
+// if (num2 > max)
+// {
+//     max = num2;
+// }
+// if (num3 > max)
+// {
+//     max = num3;
+// }
+// Console.WriteLine("Максимальное число: " +max);
 
-//                                          Задача 3
-Console.Write("Введите число: ");
-int num = int.Parse(Console.ReadLine());
+// //                                          Задача 3
+// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine());
 
-if (num % 2 == 0)
-    Console.WriteLine($"Число {num} четное");
-else
-    Console.WriteLine($"Число {num} нечетное");
+// if (num % 2 == 0)
+//     Console.WriteLine($"Число {num} четное");
+// else
+//     Console.WriteLine($"Число {num} нечетное");
 
-//                                          Задача 4
-Console.Write("Введите число: ");
-int num = int.Parse(Console.ReadLine());
+// //                                          Задача 4
+// Console.Write("Введите число: ");
+// int num = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"Четные числа от одного до {num}:");
+// Console.WriteLine($"Четные числа от одного до {num}:");
 
-for (int i = 1; i < num; i++)
+// for (int i = 1; i < num; i++)
     
-    if (i % 2 == 0)
+//     if (i % 2 == 0)
   
-    Console.Write(i + " ");
+//     Console.Write(i + " ");
