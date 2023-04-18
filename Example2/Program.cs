2723aad465a2f42bca3c3c8d3452d895f88d9cec
Shurakip <and.u8230@gmail.com>
2023-04-18 18:50:08 +0300

    Две задачи готово

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -1,4 +1,4 @@
 ﻿Console.WriteLine("Введите ваше имя");
-string username = Console.ReadLine(); //ввод данных с созданием переменной
+string username = Console.ReadLine(()); //ввод данных с созданием переменной
 Console.WriteLine("Привет, ");
 Console.Write(username);
--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//                                                  Задача 1
+// Console.WriteLine("Введите трехзначное число: ");
+// int num = int.Parse(Console.ReadLine());
+// int secondDigit = (num / 10) % 10;
+
+// Console.WriteLine("Вторая цифра числа: {0}", secondDigit);
+
+//                                                  Задача 2
+ int ThirdDigit(int number)
+        {
+            int result = -1;
+            if (number >= 100)
+            {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                result = number % 10;
+            }
+            return result; 
+        }
+        Console.Write("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+ 
+if (ThirdDigit(number1) == -1)
+Console.WriteLine("третьей цифры нет");
+else
+Console.WriteLine($"Третья цифра {ThirdDigit(number1)}");
+
+//                                                      Задача 3
