ea82945032e4897bc7df28d79f6cb80dd07b818a
Shurakip <and.u8230@gmail.com>
2023-04-18 21:45:23 +0300

    Задачи семинара4

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -6,31 +6,31 @@ int secondDigit = (num / 10) % 10;
 Console.WriteLine("Вторая цифра числа: {0}", secondDigit);
 
 //                                                  Задача 2
- int ThirdDigit(int number)
-        {
-            int result = -1;
-            if (number >= 100)
-            {
-                while (number > 999)
-                {
-                    number = number / 10;
-                }
-                result = number % 10;
-            }
-            return result; 
-        }
-        Console.Write("Введите число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
+//  int ThirdDigit(int number)
+//         {
+//             int result = -1;
+//             if (number >= 100)
+//             {
+//                 while (number > 999)
+//                 {
+//                     number = number / 10;
+//                 }
+//                 result = number % 10;
+//             }
+//             return result; 
+//         }
+//         Console.Write("Введите число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
  
-if (ThirdDigit(number1) == -1)
-Console.WriteLine("третьей цифры нет");
-else
-Console.WriteLine($"Третья цифра {ThirdDigit(number1)}");
+// if (ThirdDigit(number1) == -1)
+// Console.WriteLine("третьей цифры нет");
+// else
+// Console.WriteLine($"Третья цифра {ThirdDigit(number1)}");
 
-//                                                      Задача 3
-Console.Write("Введите цифру дня недели (1-7): ");
-int num = Convert.ToInt32(Console.ReadLine());
-    if (num == 6 || num == 7)
-        Console.WriteLine ("Это выходной");
-    else
-        Console.WriteLine ("Это рабочий!");
+// //                                                      Задача 3
+// Console.Write("Введите цифру дня недели (1-7): ");
+// int num = Convert.ToInt32(Console.ReadLine());
+//     if (num == 6 || num == 7)
+//         Console.WriteLine ("Это выходной");
+//     else
+//         Console.WriteLine ("Это рабочий!");
---
+++ Seminar4/Program.cs
@@ -0,0 +1,54 @@
+﻿//                          Задача 1 сумма чисел от 1 до А
+// System.Console.WriteLine("Введите число А: ");
+//   int A = int.Parse(Console.ReadLine());
+
+//   int getsum(int number)
+//   {
+//    int sum = 0;
+
+//   for (int i = 1; i <= A; i++)
+
+//   sum += i;
+//   return sum;
+// }
+
+// System.Console.WriteLine($"Сумма чисел от 1 до {A}^ {getsum (A)}");
+
+//                          Задача 2 подсчет цифр в числе
+// System.Console.WriteLine("Введите число: ");
+// string input = (Console.ReadLine());
+// int count = 0;
+
+// foreach (char c in input)
+// {
+//     if (Char.IsDigit(c))
+//     {
+//         count++;
+//     }
+// }
+// System.Console.WriteLine("Количество цифр в числе: " + count);
+
+//                          Задача 3 произведение чисел от 1 до N
+// System.Console.WriteLine("Введите число: ");
+// int n = int.Parse(Console.ReadLine());
+// int result = 1;
+ 
+// for (int i = 1; i <= n; i++ )
+// {
+//     result *= i;
+// }
+// Console.WriteLine($"Произведение чисел от 1 до {n} = {result}");
+
+//                          Задача 4 массив из 8 элементов, заполненный нулями и единицами в случайном порядке
+int [] array = new int [8];
+Random rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0, 2); // задается рандом от 0 до 2 (два не включительно)
+}
+Console.WriteLine("Массив из 8 элементов: ");
+foreach (int num in array)
+{
+    Console.Write($"{num}, ");
+}
