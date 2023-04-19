19bbc91c6d3a3995d0db0b305b30cd4411fc1d1b
Shurakip <and.u8230@gmail.com>
2023-04-19 18:46:25 +0300

    Домашнее задание 4 семинара

--- Seminar4/Program.cs
+++ Seminar4/Program.cs
@@ -40,15 +40,52 @@
 // Console.WriteLine($"Произведение чисел от 1 до {n} = {result}");
 
 //                          Задача 4 массив из 8 элементов, заполненный нулями и единицами в случайном порядке
-int [] array = new int [8];
-Random rnd = new Random();
+// int [] array = new int [8];
+// Random rnd = new Random();
 
-for (int i = 0; i < array.Length; i++)
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = rnd.Next(0, 2); // задается рандом от 0 до 2 (два не включительно)
+// }
+// Console.WriteLine("Массив из 8 элементов: ");
+// foreach (int num in array)
+// {
+//     Console.Write($"{num}, ");
+// }
+
+//                              Задачи домашнего задания
+//                              Задача 1 А в степени В
+// Console.Write("Введите число А: ");
+// int a = int.Parse(Console.ReadLine());
+
+// Console.Write("Введите число B: ");
+// int b = int.Parse(Console.ReadLine());
+
+// int result = 1;
+// for (int i = 0; i < b; i++)
+// {
+//     result *= a;
+// }
+// Console.WriteLine($"{a} в степени {b} = {result}");
+
+//                              Задача 2 Сумма цифр числа
+// Console.Write("Введите число: ");
+//  int num = int.Parse(Console.ReadLine());
+
+//  int sum = 0;
+//  while (num != 0)
+//  {
+//     sum += num % 10;
+//     num /= 10;
+//  }
+//  Console.WriteLine($"Сумма цифр в числе = {sum}");
+
+//                              Задача 3 
+
+int [] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+
+Console.WriteLine("Массив из 8ми элементов:");
+foreach (int num in arr)
 {
-    array[i] = rnd.Next(0, 2); // задается рандом от 0 до 2 (два не включительно)
-}
-Console.WriteLine("Массив из 8 элементов: ");
-foreach (int num in array)
-{
-    Console.Write($"{num}, ");
+    Console.Write($"{num} ");
 }
