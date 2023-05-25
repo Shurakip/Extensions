840b54b2f9b68b27b086118158a29d458e5bcf74
Shurakip <and.u8230@gmail.com>
2023-05-25 16:36:20 +0300

    Добавление ДЗ 9-го семинара.

--- Seminar9HW/Program.cs
+++ Seminar9HW/Program.cs
@@ -2,65 +2,65 @@
 // натуральные числа в промежутке от M до N.
 using System;
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Console.Write("Введите значение M: ");
-//         int m = int.Parse(Console.ReadLine());
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
 
-//         Console.Write("Введите значение N: ");
-//         int n = int.Parse(Console.ReadLine());
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
 
-//         PrintNumbers(m, n);
-//     }
+        PrintNumbers(m, n);
+    }
 
-//     static void PrintNumbers(int start, int end)
-//     {
-//         if (start <= end)
-//         {
-//             Console.Write(start + " ");
-//             PrintNumbers(start + 1, end);
-//         }
-//     }
-// }
+    static void PrintNumbers(int start, int end)
+    {
+        if (start <= end)
+        {
+            Console.Write(start + " ");
+            PrintNumbers(start + 1, end);
+        }
+    }
+}
 
 // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
 // натуральных элементов в промежутке от M до N.
 
-// using System;
+using System;
 
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Console.Write("Введите значение M: ");
-//         int m = int.Parse(Console.ReadLine());
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
 
-//         Console.Write("Введите значение N: ");
-//         int n = int.Parse(Console.ReadLine());
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
 
-//         int sum = SumOfNumbers(m, n);
+        int sum = SumOfNumbers(m, n);
 
-//         Console.WriteLine("Сумма натуральных чисел в промежутке [{0}, {1}] равна {2}", m, n, sum);
-//     }
+        Console.WriteLine("Сумма натуральных чисел в промежутке [{0}, {1}] равна {2}", m, n, sum);
+    }
 
-//     static int SumOfNumbers(int m, int n)
-//     {
-//         if (m == n)
-//         {
-//             return m;
-//         }
-//         else if (m < n)
-//         {
-//             return m + SumOfNumbers(m + 1, n);
-//         }
-//         else
-//         {
-//             return n + SumOfNumbers(m, n + 1);
-//         }
-//     }
-// }
+    static int SumOfNumbers(int m, int n)
+    {
+        if (m == n)
+        {
+            return m;
+        }
+        else if (m < n)
+        {
+            return m + SumOfNumbers(m + 1, n);
+        }
+        else
+        {
+            return n + SumOfNumbers(m, n + 1);
+        }
+    }
+}
 
 // Задача 68: Напишите программу вычисления функции Аккермана с помощью 
 // рекурсии. Даны два неотрицательных числа m и n. 
