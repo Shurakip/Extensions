5c20fd610984617eacf7d15f1b094dc31c91a1af
Shurakip <and.u8230@gmail.com>
2023-06-16 14:59:29 +0300

    Добавление программы

---
+++ Final control work/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Задание исходного массива
+        string[] inputArray = {"a", "ab", "abc", "abcd", "abcde", "abcdef"};
+
+        // Создание массива подходящих строк
+        string[] resultArray = new string[inputArray.Length];
+        int resultIndex = 0;
+
+        // Проход по исходному массиву
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            // Если строка подходит по длине, добавляем ее в результат
+            if (inputArray[i].Length <= 3)
+            {
+                resultArray[resultIndex] = inputArray[i];
+                resultIndex++;
+            }
+        }
+
+        // Сокращаем размер массива до использованной длины
+        Array.Resize(ref resultArray, resultIndex);
+
+        // Вывод результатов
+        Console.WriteLine("Входной массив: [{0}]", string.Join(", ", inputArray));
+        Console.WriteLine("Выходной массив: [{0}]", string.Join(", ", resultArray));
+        Console.ReadLine();
+    }
+}
